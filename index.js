040a39a12d602c07ca6824f35be4408d74ce932d
Pherpher089 <Christutor089@gmail.com>
2019-07-25 03:26:21 -0700

    Added in POST /api/user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./data/db')
 
 server = express();
+server.use(express.json())
 
 server.get('/api/users', (req, res) =>{
     db.find()
@@ -17,6 +18,28 @@ server.get('/api/user/:id', (req, res) => {
         res.status(200).json(user)
     })
     .catch(err => res.status(500).json(err))
+})
+
+
+server.post('/api/users/', (req, res) =>{
+    const {name, bio} = req.body;
+
+    if(!name){
+        res.status(400)
+        .json({errorMessage: 'please provide a name for the user'})
+    } else if(!bio){
+        res.status(400)
+        .json('please provide a bio for the user')
+    } else {
+        db.insert(req.body)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err =>{
+            res.status(500).json({errorMessage: `There was an error while trying to add ${req.body.name} to the data base`});
+        })
+    }
+
 })
 
 server.listen(8000, ()=> console.log('API running on port 8000'))

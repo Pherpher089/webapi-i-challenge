57db342462d63bc710096b6f6ef72c249e0a95fb
Pherpher089 <Christutor089@gmail.com>
2019-07-25 01:48:08 -0700

    Set up express API, added in GET /api/user and GET /api/user:id

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db')
+
+server = express();
+
+server.get('/api/users', (req, res) =>{
+    db.find()
+    .then(users =>{
+        res.status(200).json(users)
+    })
+    .catch(err => res.status(500).json(err)) 
+})
+
+server.get('/api/user/:id', (req, res) => {
+    db.findById(req.params.id)
+    .then(user => {
+        res.status(200).json(user)
+    })
+    .catch(err => res.status(500).json(err))
+})
+
+server.listen(8000, ()=> console.log('API running on port 8000'))
+
